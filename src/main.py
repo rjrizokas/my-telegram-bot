87e16915e2fe1b12d32e17e4ce6f307f03e176d1
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-28 01:49:24 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import WebAppInfo
 import os
+from aiohttp import web
 
 API_TOKEN = os.getenv('API_TOKEN', '7439794203:AAEQGaP_uSsTh7c5onzP1VMrLo9VO1rmmtk')
 
@@ -27,5 +28,18 @@ async def start(message: types.Message):
             url=f'https://rjrizokas.github.io/my-web-app-wordly/update1.html?user_id={user_id}')))
     await message.answer('Что наша жизнь?', reply_markup=markup)
 
+async def handle_root(request):
+    return web.Response(text="Hello! The bot is running.", content_type='text/html')
+
 if __name__ == '__main__':
+    app = web.Application()
+    app.router.add_get('/', handle_root)
+    
+    runner = web.AppRunner(app)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(runner.setup())
+    
+    site = web.TCPSite(runner, '0.0.0.0', int(os.getenv('PORT', 10000)))
+    loop.run_until_complete(site.start())
+    
     executor.start_polling(dp, skip_updates=True)
