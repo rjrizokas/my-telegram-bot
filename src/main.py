1d9c07f339d9b2af89f535469f16d13bd79e09c7
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2024-07-28 21:37:01 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,8 +22,9 @@ async def start(message: types.Message):
         url=f'https://rjrizokas.github.io/my-web-app-wordly/wordle.html?user_id={user_id}')))
     markup.add(types.KeyboardButton('слово от Ви', web_app=WebAppInfo(
         url=f'https://rjrizokas.github.io/my-web-app-wordly/wordle1.html?user_id={user_id}')))
-     markup.add(types.KeyboardButton('редактор словаря', web_app=WebAppInfo(
+    markup.add(types.KeyboardButton('редактор словаря', web_app=WebAppInfo(
         url=f'https://rjrizokas.github.io/my-web-app-wordly/add_word.html')))
+    
     if message.from_user.username in allowed_users2:
         markup.add(types.KeyboardButton('загадать слово от RJ', web_app=WebAppInfo(
             url=f'https://rjrizokas.github.io/my-web-app-wordly/update.html?user_id={user_id}')))
